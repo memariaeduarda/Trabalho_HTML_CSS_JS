ee498d2e593590bb7b26ef7280cf808b191ce7d7
Maria Eduarda <memariaeduarda0011@gmail.com>
2025-07-06 20:07:53 -0300

    Upload dos arquivos separados do Trabalho FDS
    Arquivos HTML, CSS e JS.

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const mensagem = "Mesmo que o mundo tente te esmagar e te faça duvidar de si mesmo, mantenha-se firme, pois você é forte e capaz. O que os outros pensam ou dizem sobre você não define o seu valor. 💜";
+  document.getElementById("mensagem").textContent = mensagem;
+}
